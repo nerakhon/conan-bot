838f4610fbaf76c22f322d8a24129845ebf23665
Jan Paul <paul.jan@me.com>
2020-04-03 10:44:25 +0200

    Refactoring

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='conan-bot',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='BSD',
+    author='Jan Paul',
+    author_email='paulj@nerakhon.cz',
+    description='Bot for reporting conan exiles information on discord', install_requires=['discord', 'python-a2s']
+)
