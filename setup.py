8b818f68e727fc1cd1131e0bd95ee0a5cd865ad7
Jan Paul <paul.jan@me.com>
2020-04-03 11:44:57 +0200

    Preparing for pip

--- setup.py
+++ setup.py
@@ -4,12 +4,18 @@ setup(
     name='conan_bot',
     version='1.0',
     packages=find_packages('.'),
-    url='',
-    license='BSD',
+    url='https://github.com/nerakhon/conan-bot',
+    license='MIT',
     author='Jan Paul',
     author_email='paulj@nerakhon.cz',
-    entry_points = {
+    entry_points={
         'console_scripts': ['conan-bot=conan_bot.bot:main'],
     },
-    description='Bot for reporting conan exiles information on discord', install_requires=['discord', 'python-a2s']
+    description='Bot for reporting conan exiles information on discord',
+    install_requires=['discord', 'python-a2s'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
