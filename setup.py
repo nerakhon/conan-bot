0f8885bc6a9c46dc35de8faecf3621a2aede5f22
Jan Paul <paul.jan@me.com>
2020-04-03 11:57:25 +0200

    Released on pip

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='conan_bot',
     version='1.0',
@@ -12,10 +15,13 @@ setup(
         'console_scripts': ['conan-bot=conan_bot.bot:main'],
     },
     description='Bot for reporting conan exiles information on discord',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=['discord', 'python-a2s'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
 )
